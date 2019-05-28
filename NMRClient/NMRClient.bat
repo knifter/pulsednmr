6b5ad1fac6451ae2761713cb31c1c1111d4a4657
Tijs van Roon <t.vanroon@uva.nl>
2019-05-28 18:21:30 +0200

    Installer for labs updates, qt stuff etc

--- NMRClient/NMRClient.bat
+++ NMRClient/NMRClient.bat
@@ -1,5 +1,7 @@
 @echo off
 set INSTALLDIR=C:\NMRClient
 set CONDA_PREFIX=%INSTALLDIR%\env
-set PATH=%CONDA_PREFIX%;%PATH%
+set QTDIR=%CONDA_PREFIX%\Library\bin
+set QT_QPA_PLATFORM_PLUGIN_PATH=%QTDIR%\plugins\platforms\
+set PATH=%CONDA_PREFIX%;%QTDIR%;%PATH%
 start "NMRClient Startup.." pythonw nmr.py
