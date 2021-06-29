21db9bbd4511dca57a754fffabb6cd5b648ddfa7
Tijs van Roon <knifter@gmail.com>
2021-06-29 13:01:34 +0200

    port type, output 125mhz clock and lock

--- Vivado/localcores/NMRPulseSequencer_1.1/xgui/NMRPulseSequencer_v1_1.tcl
+++ Vivado/localcores/NMRPulseSequencer_1.1/xgui/NMRPulseSequencer_v1_1.tcl
@@ -19,14 +19,3 @@ proc validate_PARAM_VALUE.US_DIVIDER { PARAM_VALUE.US_DIVIDER } {
 }
 
 
-proc update_MODELPARAM_VALUE.US_DIVIDER { MODELPARAM_VALUE.US_DIVIDER PARAM_VALUE.US_DIVIDER } {
-	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.US_DIVIDER}] ${MODELPARAM_VALUE.US_DIVIDER}
-}
-
-proc update_MODELPARAM_VALUE.US_DIVIDER_WIDTH { MODELPARAM_VALUE.US_DIVIDER_WIDTH } {
-	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	# WARNING: There is no corresponding user parameter named "US_DIVIDER_WIDTH". Setting updated value from the model parameter.
-set_property value 8 ${MODELPARAM_VALUE.US_DIVIDER_WIDTH}
-}
-
